0d3a62e63bdc369eab221e4580bfe652d9416353
jude ha <judeh@palantir.com>
2025-08-14 10:50:09 -0400

    nit: rename platformname to packageprefix

--- packages/platform-sdk-generator/src/generatePlatformSdkv2.ts
+++ packages/platform-sdk-generator/src/generatePlatformSdkv2.ts
@@ -48,8 +48,6 @@ export async function generatePlatformSdkV2(
   const componentsGenerated = new Map<Namespace, string[]>();
   const errorsGenerated = new Map<Namespace, string[]>();
 
-  const platformName = packagePrefix;
-
   // We need to make sure the components are all populated before we generate the resources
   for (const ns of model.namespaces) {
     ns.components.sort((a, b) =>
@@ -60,11 +58,11 @@ export async function generatePlatformSdkV2(
     );
     componentsGenerated.set(
       ns,
-      await generateComponents(ns, ns.paths.srcDir, platformName),
+      await generateComponents(ns, ns.paths.srcDir, packagePrefix),
     );
     errorsGenerated.set(
       ns,
-      await generateErrors(ns, ns.paths.srcDir, platformName),
+      await generateErrors(ns, ns.paths.srcDir, packagePrefix),
     );
   }
 
@@ -169,7 +167,7 @@ export async function generatePlatformSdkV2(
 export async function generateComponents(
   ns: Namespace,
   outputDir: string,
-  platformName: string = "foundry",
+  packagePrefix: string = "foundry",
 ): Promise<string[]> {
   const referencedComponents = new Set<Component>();
   const ret = [];
@@ -179,7 +177,7 @@ export async function generateComponents(
       `;
 
   for (const component of ns.components) {
-    if (isIgnoredType(component.component, platformName)) {
+    if (isIgnoredType(component.component, packagePrefix)) {
       continue;
     }
     out += component.getDeclaration(ns.name);
@@ -205,7 +203,7 @@ export async function generateComponents(
 export async function generateErrors(
   ns: Namespace,
   outputDir: string,
-  platformName: string = "foundry",
+  packagePrefix: string = "foundry",
 ): Promise<string[]> {
   const referencedComponents = new Set<Component>();
   const ret = [];
@@ -215,7 +213,7 @@ export async function generateErrors(
       `;
 
   for (const error of ns.errors) {
-    if (isIgnoredType(error.spec, platformName)) {
+    if (isIgnoredType(error.spec, packagePrefix)) {
       continue;
     }
     out += error.getDeclaration(ns.name);
