c2428195ed2ecb44b9f7749eeac83ec6755ee495
jude ha <judeh@palantir.com>
2025-08-26 11:28:48 -0400

    added docs packages

--- packages/docs-spec-platform/src/generated/spec.ts
+++ packages/docs-spec-platform/src/generated/spec.ts
@@ -709,6 +709,9 @@ export const PLATFORM_API_DOCS_SPEC = {
     "v2.getConfiguration": {
       "variables": {},
     },
+    "v2.uploadCustomJdbcDriversConnection": {
+      "variables": {},
+    },
     "v2.createFileImport": {
       "variables": {},
     },
@@ -830,6 +833,21 @@ export const PLATFORM_API_DOCS_SPEC = {
       "variables": {},
     },
     "v2.resetStream": {
+      "variables": {},
+    },
+    "v2.createModel": {
+      "variables": {},
+    },
+    "v2.getModel": {
+      "variables": {},
+    },
+    "v2.createModelVersion": {
+      "variables": {},
+    },
+    "v2.listModelVersions": {
+      "variables": {},
+    },
+    "v2.getModelVersion": {
       "variables": {},
     },
     "v2.listLogFiles": {
