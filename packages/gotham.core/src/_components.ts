a640117b570511f0361510542dc3e20665f7db2a
Nihal Bhatnagar <nihalb@palantir.com>
2025-10-13 10:51:29 -0400

    Update PR

--- packages/gotham.core/src/_components.ts
+++ packages/gotham.core/src/_components.ts
@@ -527,6 +527,7 @@ export interface LmsEmbeddingModel {
  */
 export type LmsEmbeddingModelValue =
   | "OPENAI_TEXT_EMBEDDING_ADA_002"
+  | "TEXT_EMBEDDING_3_LARGE"
   | "TEXT_EMBEDDING_3_SMALL"
   | "SNOWFLAKE_ARCTIC_EMBED_M"
   | "INSTRUCTOR_LARGE"
