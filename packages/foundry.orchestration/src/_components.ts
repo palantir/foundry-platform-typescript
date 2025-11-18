fb159c9a0e118c45765afb391e22fdf671bfa98b
Nihal Bhatnagar <nihal.bhatnagar@gmail.com>
2025-11-18 12:27:55 -0500

    Regenerate Platform SDKs (#223)
    * Version Packages
    
    * Regen
    
    * Fix
    
    * rebase

--- packages/foundry.orchestration/src/_components.ts
+++ packages/foundry.orchestration/src/_components.ts
@@ -398,6 +398,7 @@ export interface Job {
   rid: _Core.JobRid;
   buildRid: _Core.BuildRid;
   startedTime: JobStartedTime;
+  latestAttemptStartTime?: string;
   finishedTime?: string;
   jobStatus: JobStatus;
   outputs: Array<JobOutput>;
